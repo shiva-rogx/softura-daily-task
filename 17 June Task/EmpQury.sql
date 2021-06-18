070e693e630176041105a6585685f3dfc6edc6f1
shiva-rogx <65455314+shiva-rogx@users.noreply.github.com>
2021-06-18 10:09:43 +0530

    Update EmpQury.sql

--- 17 June Task/EmpQury.sql
+++ 17 June Task/EmpQury.sql
@@ -3,6 +3,7 @@ use AdventureWorks2017
 
 select *from Person.Person
 
+
 select DepartmentID,Name from HumanResources.Department 
 
 select * from HumanResources.Employee
@@ -10,7 +11,8 @@ select * from HumanResources.Employee
 select * from HumanResources.EmployeeDepartmentHistory
 
 
-select e.BusinessEntityID,p.FirstName,e.BirthDate,e.MaritalStatus,e.Gender,e.HireDate from HumanResources.EmployeeDepartmentHistory dh join HumanResources.Department d 
+select e.BusinessEntityID,p.FirstName,e.BirthDate,e.MaritalStatus,e.Gender,e.HireDate 
+from HumanResources.EmployeeDepartmentHistory dh join HumanResources.Department d 
 on d.DepartmentID=dh.DepartmentID join HumanResources.Employee e on e.BusinessEntityID=dh.BusinessEntityID 
 join Person.Person p on p.BusinessEntityID=e.BusinessEntityID
-where d.DepartmentID = 2
+where d.DepartmentID = 6
