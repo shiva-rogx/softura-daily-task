500d1ca15e4bbaacf8d7c3a8b7b7a2f304117306
shiva-rogx <65455314+shiva-rogx@users.noreply.github.com>
2021-06-23 10:17:03 +0530

    Exception Log file

---
+++ June 22 Task/MVCusingDPR/Controllers/BookController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCusingDPR.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using Dapper;
+using System.Diagnostics;
+
+namespace MVCusingDPR.Controllers
+{
+    [ExceptionFilter]
+    public class BookController : Controller
+    {
+        // GET: Book
+        [HandleError]
+        public ActionResult Index()
+        {
+            List<BookModel> BKlist = new List<BookModel>();
+            using(IDbConnection dbcon=new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+
+                BKlist = dbcon.Query<BookModel>("select * from tbl_Books").ToList();
+                throw new Exception("Not working");
+            }
+            return View(BKlist);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            BookModel bk = new BookModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<BookModel>("select * from tbl_Books where BookId="+id,new {id}).SingleOrDefault();
+            }
+            return View(bk);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(BookModel bmodel)
+        {
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                string query = "insert into tbl_Books(Title,AuthorId,Price) "+
+                    "values('" + bmodel.Title + "'," + bmodel.AuthorId + "," + bmodel.Price + ")";
+                int rowIns = dbcon.Execute(query);
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            BookModel bookModel = new BookModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bookModel = dbcon.Query<BookModel>("select * from tbl_Books where BookId=" + id).SingleOrDefault();
+            }
+            return View(bookModel);
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, BookModel bmodel)
+        {
+            try
+            {
+                using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+                {
+                    string query = "update tbl_Books " +
+                        "set Title='" + bmodel.Title + "',AuthorId=" + bmodel.AuthorId + ",Price=" + bmodel.Price + " where BookId="+id;
+                    int rowIns = dbcon.Execute(query);
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            BookModel bookModel = new BookModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bookModel = dbcon.Query<BookModel>("select * from tbl_Books where BookId=" + id).SingleOrDefault();
+            }
+            return View(bookModel);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, BookModel bookModel)
+        {
+            BookModel bk = new BookModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<BookModel>("delete from tbl_Books where BookId=" + id).SingleOrDefault();
+            }
+            return RedirectToAction("Index");           
+        }
+    }
+    class ActionFilter : ActionFilterAttribute, IActionFilter
+    {
+        void IActionFilter.OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //After action method executes
+            Trace.WriteLine("Method executed sucessfully" + DateTime.Now.ToString());
+        }
+        void IActionFilter.OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //Before action method executes
+            var ctrlName = filterContext.RouteData.Values["controller"];
+            var methodName = filterContext.RouteData.Values["action"];
+            Trace.WriteLine("Method  is executing" + DateTime.Now.ToString() + " from the controller " + ctrlName.ToString() + " from the method " + methodName.ToString());
+            //filterContext.Result = new RedirectResult("https://roli.com/");
+
+        }
+    }
+    public class ExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {            
+                var ctrlName = filterContext.RouteData.Values["controller"];
+                var methodName = filterContext.RouteData.Values["action"];
+                Trace.WriteLine("Exception occurs " + DateTime.Now.ToString() + " from the controller " + ctrlName.ToString() + " from the method " + methodName.ToString());
+                filterContext.Result = new RedirectResult("https://roli.com/");
+        }
+    }
+}
---
+++ June 22 Task/MVCusingDPR/Models/BookModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCusingDPR.Models
+{
+    public class BookModel
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string AuthorId { get; set; }
+        public string Price { get; set; }
+    }
+}
